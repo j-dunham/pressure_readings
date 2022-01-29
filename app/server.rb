8fe8228d6acc769abec130fb1771c705274a40e1
j-dunham <jmdunham20@gmail.com>
2022-01-29 11:09:07 -0500

    add db connection

--- app/server.rb
+++ app/server.rb
@@ -4,8 +4,11 @@ require 'rack'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'dotenv'
+require 'sequel'
 
 Dotenv.load
+
+DB = Sequel.connect(ENV['DATABASE_URL'])
 
 configure do
   set :public_folder, "#{__dir__}/static"
@@ -16,13 +19,14 @@ get '/' do
   erb :index, locals: { message: 'Welcome<br>To<br>Null Island<br>' }
 end
 
-get '/location' do
+get '/api/locations' do
   status 401
   return erb :not_allow unless validate_token(request)
 
   status 200
   content_type 'application/json'
-  { 'location' => '0,0' }.to_json
+
+  DB[:locations].all.to_json
 end
 
 not_found do
